dce46d67c83785ed0a336b4dd0d890c147db5e97
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-03-10 11:00:41 -0300

    test orm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecadosController } from 'src/recados/recados.controller';
+import { RecadosModule } from 'src/recados/recados.module';
 import { RecadosService } from 'src/recados/recados.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 
 @Module({
-  imports: [],
-  controllers: [RecadosController],
-  providers: [RecadosService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username:'postgres',
+    database: 'postgres',
+    password:'123',
+    autoLoadEntities: true, //carrega entidades sem precisar especifica-las
+    synchronize: true, // sincroniza com o BD. Não deve ser usado em produção
+  }),
+    RecadosModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
