f78ba767d1f7d9dabe1c69ac257097fe671892cf
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-08-02 14:32:55 -0300

    auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { Global, Module } from "@nestjs/common";
 import { BcryptService } from "src/hashing/bcrypy.service";
 import { HashingService } from "src/hashing/hashing.service";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Global()
 @Module({
+    controllers: [AuthController],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthService,
   ],
   exports: [HashingService],
 })
---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class LoginDto {
+
+    @IsEmail()
+    email: string | undefined;
+
+     @IsString()
+    @IsNotEmpty()
+    password: string | undefined;
+
+}
+
