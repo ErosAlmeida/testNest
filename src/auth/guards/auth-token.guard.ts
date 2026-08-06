6c9ad9e1ba14618642016fde2b78e8625d96b321
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-08-06 11:23:18 -0300

    guard

---
+++ src/auth/guards/auth-token.guard.ts
@@ -0,0 +1,47 @@
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { REQUEST_TOKEN_PAYLOAD_KEY } from "../auth.constants";
+import { JwtService } from "@nestjs/jwt";
+import jwtConfig from "../config/jwt.config";
+import * as config from "@nestjs/config";
+
+
+@Injectable()
+export class AuthTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: config.ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Não logado!');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+
+      request[REQUEST_TOKEN_PAYLOAD_KEY] = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido!');
+    }
+
+    return true;
+  }
+
+  extractTokenFromHeader(request: Request): string | undefined {
+    const authorization = request.headers;
+
+    if (!authorization || typeof authorization !== 'string') {
+      return;
+    }
+
+    return authorization;
+  }
+}
--- src/pessoas/pessoa.controller.ts
+++ src/pessoas/pessoa.controller.ts
@@ -8,6 +8,8 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  Req,
+  UseGuards,
 } from "@nestjs/common";
 import { PessoasService } from "./pessoa.service";
 import { CreatePessoaDto } from "./dto/create-pessoa.dto";
@@ -15,6 +17,10 @@ import { CreatePessoaDto } from "./dto/create-pessoa.dto";
 import { UpdatePessoaDto } from "./dto/update-pessoa.dto";
 import { REMOVE_SPACES_REGEX } from "src/common/constants/server-name.constant";
 import { LetterLower } from "src/common/regex/letterLower";
+import { AuthTokenGuard } from "src/auth/guards/auth-token.guard";
+import { REQUEST_TOKEN_PAYLOAD_KEY } from "src/auth/auth.constants";
+
+@UseGuards(AuthTokenGuard)
 @Controller("pessoas")
 export class PessoasController {
   onlyLowercaseLettersRegex: any;
@@ -25,16 +31,17 @@ export class PessoasController {
   
   }
 
+  @Get()
+  findAll(@Req() request: Request) {
+    console.log(request[REQUEST_TOKEN_PAYLOAD_KEY].sub);
+    return this.pessoasService.findAll();
+  }
+
   @Post()
   create(@Body() createPessoaDto: CreatePessoaDto) {
     return this.pessoasService.create(createPessoaDto);
   }
 
-  @Get()
-  findAll() {
-    return this.pessoasService.findAll();
-    console.log(this.onlyLowercaseLettersRegex.execute("TESTE"));
-  }
 
   @Get(":id")
   findOne(
