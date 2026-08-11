9df03049718826bb742ab307fa856ccdc4a87728
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-08-10 21:13:09 -0300

    recados

---
+++ src/recados/recados.service.spec.ts
@@ -0,0 +1,29 @@
+import { Repository } from "typeorm";
+import { Recado } from "./entities/recados.entity";
+import { RecadosService } from "./recados.service";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+describe('RecadosService', () => {
+    let recadosService: RecadosService;
+    let recadosRepository: Repository<Recado>
+})
+beforeEach(async () => {
+    const module: TestingModule = Test.createTestingModule({
+        providers: [
+            RecadosService, {
+                provide: getRepositoryToken(Recado),
+                useValue: {},
+            }
+        ]
+    })
+
+    recadosService = module.get<RecadosService>(RecadosService)
+     recadosRepository = module.get<Repository<Recado>>(
+        getRepositoryToken(Recado)
+    )
+
+    it('recadosService deve estar definido', () => {
+        expect(recadosService).toBeDefined();
+    });
+});
